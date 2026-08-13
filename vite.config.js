c8483b91138f345aac1ed360959833262302d611
expreali977-commits <expreali977@gmail.com>
2026-08-14 01:51:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   server: {
@@ -7,5 +8,23 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        create: path.resolve(__dirname, 'create.html'),
+        liquidity: path.resolve(__dirname, 'liquidity.html'),
+        faq: path.resolve(__dirname, 'faq.html'),
+        wallet: path.resolve(__dirname, 'wallet.html'),
+      }
+    }
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  // Importante: ottimizza le dipendenze
+  optimizeDeps: {
+    include: ['@walletconnect/web3wallet']
+  }
 });
