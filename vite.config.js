5c9056390aa1951c7a3944503130834f734bc91f
expreali977-commits <expreali977@gmail.com>
2026-08-14 02:54:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,13 @@ export default defineConfig({
     include: [
       '@web3modal/ethers',
       '@web3modal/core',
-      '@walletconnect/universal-provider'
+      '@walletconnect/universal-provider',
+      'react',
+      'react-dom'
     ]
+  },
+  // Forza l'uso di React 18 per evitare conflitti
+  define: {
+    'process.env.NODE_ENV': '"production"',
   }
 });
